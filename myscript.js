78623d0ceb4eda3538548afd4f56e28098167c5c
Shelbot77 <alessiodeangelis021@gmail.com>
2022-11-30 16:44:10 +0100

    Inserimento quadrati con ciclo for

--- myscript.js
+++ myscript.js
@@ -1,18 +1,17 @@
 
-//recupero il container generale dal dom
+
 const mainContainer = document.getElementById('wrapper');
 
-//creo un div da inserire nel container generale
-const newDiv = document.createElement('div');
-mainContainer.append(newDiv);
 
-//attribuisco al div contenuto della classe che voglio
-newDiv.classList.add('square');
 
 
 
 for (let i = 1; i <= 100; i++){
     console.log(i);
+    const newDiv = document.createElement('div');
+    mainContainer.append(newDiv);
+    newDiv.classList.add('square');
+    
     
 
 }
