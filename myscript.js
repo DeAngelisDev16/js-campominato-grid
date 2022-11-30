042e81b5b233164b2f326effb16f77e93e8de2f6
Shelbot77 <alessiodeangelis021@gmail.com>
2022-11-30 17:17:45 +0100

    addEventListener caselle numerate

--- myscript.js
+++ myscript.js
@@ -11,11 +11,15 @@ for (let i = 1; i <= 100; i++){
     console.log(i);
     const newDiv = document.createElement('div');
     newDiv.classList.add('square');
+    newDiv.addEventListener("click", function(){
+        newDiv.classList.toggle('click');
+    })  
+
     mainContainer.append(newDiv);
     newDiv.append(i);
 }
     
-    
+   
 
 
     
