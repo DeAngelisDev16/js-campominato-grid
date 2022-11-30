10cf3060b91135f4af052480da98a0150a715602
Shelbot77 <alessiodeangelis021@gmail.com>
2022-11-30 17:52:17 +0100

    Il gioco appare e scompare al click di play

--- myscript.js
+++ myscript.js
@@ -2,26 +2,28 @@
 
 const mainContainer = document.getElementById('wrapper');
 const playButton = document.getElementById('play');
-mainContainer.classList.add('d-none');
+
 
 
 playButton.addEventListener("click", function(){
-    mainContainer.classList.remove('d-none');
+    mainContainer.innerHTML= '';
+    for (let i = 1; i <= 100; i++){
+        
+        const newDiv = document.createElement('div');
+        newDiv.classList.add('square');
+        newDiv.addEventListener("click", function(){
+            newDiv.classList.toggle('click');
+            console.log(newDiv);
+        })  
+        
+        mainContainer.append(newDiv);
+        newDiv.append(i);
+    }
+    
 })
 
 
 
-for (let i = 1; i <= 100; i++){
-    console.log(i);
-    const newDiv = document.createElement('div');
-    newDiv.classList.add('square');
-    newDiv.addEventListener("click", function(){
-        newDiv.classList.toggle('click');
-    })  
-    
-    mainContainer.append(newDiv);
-    newDiv.append(i);
-}
 
 
 
